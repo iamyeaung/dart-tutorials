2cdeeef8b5c6c7ab0a68da5d1ad841747171bd40
Ye Aung <khunyeaung@gmail.com>
2024-08-23 12:00:36 +0630

    Print Name

---
+++ bin/01_basic_knowledge/02_print_name.dart
@@ -0,0 +1,14 @@
+/* Basic Dart Program Explained
+    void main() is the starting point where the execution of your program begins.
+    Every program starts with a main function.
+    The curly braces {} represent the beginning and the ending of a block of code.
+    print(“Hello World!”); prints Hello World! on screen.
+    Each code statement must end with a semicolon.
+*/
+
+// Basic Dart Program For Printing Name
+
+void main() {
+  var name = "Jone";
+  print(name);
+}
