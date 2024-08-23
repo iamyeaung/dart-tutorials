0649edb43a9272fd6d3be41fa312094193defd90
Ye Aung <khunyeaung@gmail.com>
2024-08-23 11:35:19 +0630

    Hello World

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 # dart-tutorials
 Dart Tutorials
+=======
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
+>>>>>>> d9b9f82 (Hello World)
---
+++ bin/01_basic_knowledge/01_hello_world.dart
@@ -0,0 +1,7 @@
+// Basic Dart Program
+// This is a simple dart program that prints Hello World on screen. 
+// Most programmers write the Hello World program as their first program.
+
+void main() {
+  print('Hello World');
+}
